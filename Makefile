79fc44f8820f9be61c773b2a8060a9bc1c29d0aa
BredyByte <correodemimobil@gmail.com>
2024-05-03 15:22:17 +0200

    fix: Images building is fixed. WP fixed

--- Makefile
+++ Makefile
@@ -11,30 +11,22 @@ REQ_SRC = $(SRCS)/requirements
 RM = docker rm
 DOCKER_COMPOSE_FILE = $(SRCS)/docker-compose.yml
 
-NGINX_I_NAME = nginx
-WP_I_NAME = wordpress
+NGINX_I_NAME = srcs-nginx
+WP_I_NAME = srcs-wordpress
 
 NETWORK_NAME = webnet
-
-NGINX_BUILD = docker build -t $(NGINX_I_NAME) $(REQ_SRC)/nginx
-WP_BUILD = docker build -t $(WP_I_NAME) $(REQ_SRC)/wordpress
 
 .PHONY: all build up down fcrean network re
 .SILENT: all build up down fclean network re
 
 all: up
 
-build:
-	echo "$(MATCHING_COLOR)$(U_LINE)Images: Building...$(RESET) \n"
-	$(NGINX_BUILD) && $(WP_BUILD)
-	echo "\n$(TOXIC_GREEN)$(U_LINE)🧪 Images: Created 🧪$(RESET) \n";
-
 network:
 	if ! docker network inspect $(NETWORK_NAME) >/dev/null 2>&1; then \
         docker network create -d bridge $(NETWORK_NAME); \
     fi
 
-up: build network
+up: network
 	docker compose -f $(DOCKER_COMPOSE_FILE) up -d
 	echo "$(MATCHING_COLOR)$(U_LINE)Docker compose is running in deteched mode $(RESET) \n"
 
--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
+wget http://wordpress.org/latest.tar.gz
+tar xfz latest.tar.gz
+mv wordpress/* .
+rm -rf latest.tar.gz
+rm -rf wordpress
 
-if [ -f ./wp-config.php ]; then
-    echo "WordPress already configured."
-else
-    wget http://wordpress.org/latest.tar.gz && \
-    tar xfz latest.tar.gz && \
-    mv wordpress/* . && \
-    rm -rf latest.tar.gz wordpress
+sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php && \
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php && \
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php && \
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php && \
-    cp wp-config-sample.php wp-config.php
+cp wp-config-sample.php wp-config.php
 
-    echo "WordPress configured."
-fi
+echo "WordPress configured."
+
+/usr/sbin/php-fpm82 -F
 
 exec "$@"
