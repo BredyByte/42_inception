702c5290cd0275b35080b8c14282e157b499ba95
BredyByte <correodemimobil@gmail.com>
2024-05-26 14:07:44 +0200

    refactor: preformance is updated

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -13,13 +13,15 @@ sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 
 cp wp-config-sample.php wp-config.php
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+    && chmod +x wp-cli.phar \
+    && mv wp-cli.phar /usr/local/bin/wp
 
 chmod +x /usr/local/bin/wp
 
-php82 /usr/local/bin/wp core install \
+ln -s /usr/bin/php82 /usr/bin/php
+
+wp core install \
     --allow-root \
     --url=$DOMAIN_NAME \
     --title="HELLOGENTUZA" \
@@ -29,20 +31,15 @@ php82 /usr/local/bin/wp core install \
     --skip-email \
     --path=/var/www/html \
 
-php82 /usr/local/bin/wp user create \
+wp user create \
     --allow-root \
     $WP_USER '$WP_USER@gmail.com' \
     --user_pass=$WP_PASSWORD \
     --path=/var/www/html \
     --url=$DOMAIN_NAME
 
+media_id=$(wp media import /var/www/uploads/inception.webp --title="Dumb too(2) ;)" --porcelain)
 
-php82 /usr/local/bin/wp core language install en_US
-php82 /usr/local/bin/wp site switch-language en_US
-
-media_id=$(php82 /usr/local/bin/wp media import /var/www/uploads/inception.webp --title="Dumb too(2) ;)" --porcelain)
-
-php82 /usr/local/bin/wp post meta set 1 _thumbnail_id "$media_id"
-
+wp post meta set 1 _thumbnail_id "$media_id"
 
 /usr/sbin/php-fpm82 -F
